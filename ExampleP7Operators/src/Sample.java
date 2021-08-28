c225cf1ad34d90f13f97305b6f554e17e766b621
OnlineTrainer <mohamedtechdata@gmail.com>
2021-08-28 13:43:24 +0530

    28-08-2021

---
+++ ExampleP7Operators/src/Sample.java
@@ -0,0 +1,39 @@
+class Demo
+{
+	void arithmeticOperators()
+	{
+		int a=10,b=20;
+		System.out.println("Addition::"+(a+b));
+		System.out.println("Divided::"+(a/b));
+		System.out.println("Mod..::"+(a%b));
+		
+	}
+	void comparisionOperator()
+	{
+		int a1=100,b1=20;
+		System.out.println("Lessthan<= is::"+(a1<b1));
+		System.out.println("greaterthan<= is::"+(a1>b1));
+		System.out.println("Equalto== is::"+(a1==b1));
+	}
+	void logicalOperators()
+	{
+		int a2=100,b2=200;
+		System.out.println("LogicalAnd::"+((a2<b2)&&(b2>a2)));//True
+		System.out.println("LogicalAnd::"+((a2>b2)&&(b2>a2)));//False
+		System.out.println("LogicalOR::"+((a2<b2)||(b2>a2)));//True
+		System.out.println("LogicalNOT::"+!(a2<b2));//False
+	}
+	
+}
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		Demo f1=new Demo();
+		//f1.arithmeticOperators();
+		//f1.comparisionOperator();
+		f1.logicalOperators();
+
+	}
+
+}
