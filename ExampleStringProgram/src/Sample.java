1161458614dbbea7222ec488bf103f455e08ae32
OnlineTrainer <mohamedtechdata@gmail.com>
2021-09-07 19:44:39 +0530

    07-09-2021

---
+++ ExampleStringProgram/src/Sample.java
@@ -0,0 +1,30 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+//		String s1="welcome";
+//		String s2=new String("hello");
+//		System.out.println("Result is::"+s1+""+s2);
+//		System.out.println("Length of the String is::"+s1.length());
+		
+		/*String s1="admin";
+		String s2="admin@123";
+		System.out.println(s1.equals(s2));
+		if(s1.equals(s2))
+		{
+			System.out.println("LoginSuccess!!!");
+		}
+		else
+		{
+			System.out.println("LoginFail!!!");
+		}*/
+		
+        String str=new String("Welcome to Java");
+		
+		System.out.println(str.startsWith("A"));
+		System.out.println(str.startsWith("W"));
+		
+	}
+
+}
